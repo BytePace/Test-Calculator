5a0d971ef3e761ce88a4d4147ae1cf10c9f71eca
DESKTOP-ACHM9AP\vikto <viktor.matskevich@sy-dev.com>
2017-07-11 14:43:10 +0600

    first upload project

---
+++ app/src/androidTest/java/com/bytepace/test/calculator/MainActivityTest.java
@@ -0,0 +1,75 @@
+package com.bytepace.test.calculator;
+
+import android.support.test.rule.ActivityTestRule;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+
+/**
+ * Created by vikto on 10-Jul-17.
+ */
+public class MainActivityTest extends Assert {
+
+    private MainActivity mMainActivity = null;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        mMainActivity = mActivityTestRule.getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mMainActivity = null;
+    }
+
+    @Test
+    public void testCleanData_input2plus2_allObjectsCleaned() {
+        assertNotNull(mMainActivity);
+
+        final Button addButton = mMainActivity.getAddButton();
+        final EditText inputDataEditText = mMainActivity.getInputDataEditText();
+
+        mMainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                inputDataEditText.setText(String.valueOf(2));
+                addButton.performClick();
+                inputDataEditText.setText(String.valueOf(2));
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+
+        assertEquals(inputDataEditText.getText().toString(), "2");
+        assertEquals(mMainActivity.getTextResult(), "2.0 +");
+        assertTrue(mMainActivity.mFirstValue == 2);
+        assertTrue(mMainActivity.mSecondValue == 0);
+
+        final Button cleanButton = mMainActivity.getCleanButton();
+
+        mMainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                cleanButton.performClick();
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+
+        assertEquals(mMainActivity.getInputDataEditText().getText().toString(), "");
+        assertEquals(mMainActivity.getTextResult(), "");
+        assertTrue(mMainActivity.mFirstValue == 0);
+        assertTrue(mMainActivity.mSecondValue == 0);
+    }
+}
---
+++ app/src/main/java/com/bytepace/test/calculator/MainActivity.java
@@ -0,0 +1,176 @@
+package com.bytepace.test.calculator;
+
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mInputDateEditText;
+    private Button mCalculateButton;
+    private Button mAddButton;
+    private Button mSubtractButton;
+    private Button mDivideButton;
+    private Button mMultiplyButton;
+    private Button mCleanButton;
+    private TextView mResultTextView;
+
+    @VisibleForTesting
+    public float mFirstValue = 0, mSecondValue = 0;
+    @VisibleForTesting
+    public Operation mOperation = null;
+
+    public String ADD;
+    public String SUBTRACT;
+    public String DIVIDE;
+    public String MULTIPLY;
+
+    private enum Operation {
+        ADD,
+        SUBTRACT,
+        DIVIDE,
+        MULTIPLY
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mInputDateEditText = (EditText) findViewById(R.id.et_input_data);
+        mCalculateButton = (Button) findViewById(R.id.btn_calculate);
+        mAddButton = (Button) findViewById(R.id.btn_add);
+        mSubtractButton = (Button) findViewById(R.id.btn_subtract);
+        mDivideButton = (Button) findViewById(R.id.btn_divide);
+        mMultiplyButton = (Button) findViewById(R.id.btn_multiply);
+        mCleanButton = (Button) findViewById(R.id.btn_clean);
+        mResultTextView = (TextView) findViewById(R.id.tv_result);
+
+        mCalculateButton.setOnClickListener(this);
+        mAddButton.setOnClickListener(this);
+        mSubtractButton.setOnClickListener(this);
+        mDivideButton.setOnClickListener(this);
+        mMultiplyButton.setOnClickListener(this);
+        mCleanButton.setOnClickListener(this);
+
+        ADD = getString(R.string.operation_add);
+        SUBTRACT = getString(R.string.operation_add);
+        DIVIDE = getString(R.string.operation_add);
+        MULTIPLY = getString(R.string.operation_add);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String strValue =  mInputDateEditText.getText().toString();
+        if(strValue.equals("") && v.getId() != R.id.btn_clean)
+            return;
+        else if(v.getId() == R.id.btn_clean) {
+            cleanData();
+            return;
+        }
+
+        float currentValue = Float.valueOf(strValue);
+        switch (v.getId()) {
+            case R.id.btn_add :
+                setValues(currentValue, Operation.ADD);
+                break;
+            case R.id.btn_subtract :
+                setValues(currentValue, Operation.SUBTRACT);
+                break;
+            case R.id.btn_divide :
+                setValues(currentValue, Operation.DIVIDE);
+                break;
+            case R.id.btn_multiply :
+                setValues(currentValue, Operation.MULTIPLY);
+                break;
+            case R.id.btn_calculate :
+                if(mOperation != null) {
+                    mSecondValue = currentValue;
+                    calculate();
+                }
+                break;
+        }
+    }
+
+    private void calculate() {
+        switch (mOperation) {
+            case ADD:
+                setResult(mFirstValue + mSecondValue, ADD);
+                break;
+            case SUBTRACT:
+                setResult(mFirstValue - mSecondValue, SUBTRACT);
+                break;
+            case DIVIDE:
+                setResult(mFirstValue / mSecondValue, DIVIDE);
+                break;
+            case MULTIPLY:
+                setResult(mFirstValue * mSecondValue, MULTIPLY);
+                break;
+        }
+    }
+
+    private void cleanData() {
+        mInputDateEditText.setText("");
+        mResultTextView.setText("");
+        mFirstValue = 0;
+        mSecondValue = 0;
+        mOperation = null;
+    }
+
+    private void setValues(float value, Operation operation) {
+        mFirstValue = value;
+        mOperation = operation;
+        mResultTextView.setText(mFirstValue + " " + getStringFromOperation(operation));
+        mInputDateEditText.setText("");
+    }
+
+    private void setResult(float result, String operation) {
+        mResultTextView.setText(mFirstValue + " " + operation + " " + mSecondValue + " = " + result);
+        mInputDateEditText.setText("");
+        mOperation = null;
+    }
+
+    private String getStringFromOperation(Operation operation) {
+        switch (operation) {
+            case ADD:
+                return ADD;
+            case SUBTRACT:
+                return SUBTRACT;
+            case DIVIDE:
+                return DIVIDE;
+            case MULTIPLY:
+                return MULTIPLY;
+            default:
+                return "";
+        }
+    }
+
+    @VisibleForTesting
+    public String getTextInputData() {
+        return mInputDateEditText.getText().toString();
+    }
+
+    @VisibleForTesting
+    public String getTextResult() {
+        return mResultTextView.getText().toString();
+    }
+
+    @VisibleForTesting
+    public Button getAddButton() {
+        return mAddButton;
+    }
+
+    @VisibleForTesting
+    public EditText getInputDataEditText() {
+        return mInputDateEditText;
+    }
+
+    @VisibleForTesting
+    public Button getCleanButton() {
+        return mCleanButton;
+    }
+}
